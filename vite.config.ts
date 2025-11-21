4102c1cdd534c3a7ccba9497ee99157935edd274
Allan Juliani <allan.s.juliani@gmail.com>
2025-11-20 23:40:22 -0300

    chore: configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: '/allanjuliani/',
+    base: '/',
     build: {
       target: 'esnext',
       outDir: 'build',
